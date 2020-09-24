d61347482670952a27f5f7bdb24f05afcf68129f
Regina <saifieva98@gmail.com>
2020-09-25 00:38:02 +0300

    various conclusions of the examples

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+const nam = 5;
+const flag = true;
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,26 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p style = {{
+        		color: 'red'
+        }}>
+        	Hello World 
+        </p>
+        <p>
+        	{nam}
+        </p>
+        <p>{789}</p>
+        <p>{ Math.pow(2,3)}</p>
+        <p>
+        {flag && 'Flag is true'}
+        </p>
+        <p>
+        	{flag ? 'Flag is true' : 'Flag is false'}
+        	{undefined}
+        	{null}
+        	{false}
+        	{true}
         </p>
         <a
           className="App-link"
